df41da5ee1d20032a046f621f57f6f72128ea81c
DeusdedithNathan <deusd2070@gmail.com>
2026-01-31 20:19:02 +0300

    creating repository

--- app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
+++ app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
@@ -10,4 +10,5 @@ class StudentRepository(private val dao: StudentDao) {
     suspend fun insert(student: Student) = dao.insertStudent(student)
     suspend fun update(student: Student) = dao.updateStudent(student)
     suspend fun delete(student: Student) = dao.deleteStudent(student)
+
 }
