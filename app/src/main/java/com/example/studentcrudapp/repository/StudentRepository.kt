05384ba9cfce9857c34b05e39d53a48f9d3bdb6a
DeusdedithNathan <deusd2070@gmail.com>
2026-02-01 23:59:33 +0300

    modifications cont

--- app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
+++ app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
@@ -11,4 +11,5 @@ class StudentRepository(private val dao: StudentDao) {
     suspend fun update(student: Student) = dao.updateStudent(student)
     suspend fun delete(student: Student) = dao.deleteStudent(student)
 
+
 }
