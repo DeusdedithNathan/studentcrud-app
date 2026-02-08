6f01e17cc62db2de3040570acec1228303d0a4a5
DeusdedithNathan <deusd2070@gmail.com>
2026-02-08 23:06:33 +0300

    modifications

--- app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
+++ app/src/main/java/com/example/studentcrudapp/repository/StudentRepository.kt
@@ -8,4 +8,5 @@ import kotlinx.coroutines.flow.Flow
 class StudentRepository (private val dao: StudentDao){
  val allStudents: Flow<List<Student>> = dao.getAllStudents()
     suspend fun insert(student:Student) = dao.insertStudent(student)
+
 }
